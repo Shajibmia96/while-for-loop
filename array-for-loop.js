e159927fd50531bffed7c3fb60a803b56e0a6bf1
Shajib96 <shajibahamedornob@gmail.com>
2023-09-22 23:51:22 +0600

    add array for loop

---
+++ array-for-loop.js
@@ -0,0 +1,19 @@
+var numbers = [23,43,54,65,23,54,64,67,11,34,54,32,43,42,42,67,54,75,75]
+for(i =0; i<numbers.length ;i++){
+    number = numbers[i]
+    console.log(number)
+}
+
+
+var friends = ['Akash', 'Tipu','Sayem','Arman','Tanvir','Amir','Sagor','Mainul']
+for(i=0; i<friends.length;i++){
+    var friend = friends[i]
+    console.log(friend)
+}
+
+
+var fruitNames =['mango','apple','orange','banana','jackfruits','etc']
+for(i=0; i<fruitNames.length; i++){
+    var fruit= fruitNames[i];
+    console.log(fruit)
+}
