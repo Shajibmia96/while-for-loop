2a201c16d37999d302026041975ac9c783706800
Shajib96 <shajibahamedornob@gmail.com>
2023-09-23 00:29:22 +0600

    Know about break

---
+++ break.js
@@ -0,0 +1,32 @@
+for(i=0; i<20;i++){
+    console.log(i)
+    if(i>10){
+        break
+    }
+}
+var i=0;
+while(i<20){
+    console.log(i);
+    if(i>10){
+        break;
+    }
+    i++
+}
+
+var friends = ['Akash', 'Tipu','Sayem','Arman','Tanvir','Amir','Sagor','Mainul']
+for(i=0; i<friends.length;i++){
+    var friend = friends[i]
+    console.log(friend)
+    if(friend=='Tanvir'){
+        break;
+    }
+}
+var numbers =[435,364,754,643,0,467,864,755,865,75];
+for(i=0; i<numbers.length; i++){
+    number =numbers[i];
+    console.log(number);
+    if(number==0){
+        break;
+    }
+        
+}
