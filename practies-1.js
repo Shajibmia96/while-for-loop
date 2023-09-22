4220e09ec0e7c547f37db7be8e2cdaae4e4b9df6
Shajib96 <shajibahamedornob@gmail.com>
2023-09-23 02:06:00 +0600

    Practies some mathmeties problem

---
+++ practies-1.js
@@ -0,0 +1,19 @@
+var momGiven =1000;
+var applePrice = 400;
+var orangePrice = 300;
+var expensive = applePrice+orangePrice;
+console.log("Total" ,expensive)
+var shopCopperReturn= momGiven - expensive ;
+console.log("Return",shopCopperReturn);
+
+var math= 78;
+var chemi = 87.15;
+var physics = 77.69;
+var bio = 66;
+var bangla = 99.50
+var totalMarks = math+chemi+physics+bio+bangla;
+
+var average= totalMarks / 5;
+ averageTwoDecimal = average.toFixed(2)
+ var switchNumber = parseFloat(averageTwoDecimal)
+console.log(switchNumber)
