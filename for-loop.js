b8fa358b1cc0cc8b00c2a7bb21d4c8fc8a23870a
Shajib96 <shajibahamedornob@gmail.com>
2023-09-22 12:23:49 +0600

    for loop section done

---
+++ for-loop.js
@@ -0,0 +1,21 @@
+var number =1;
+while(number/3 ,number < 100  ){
+    console.log(number)
+    number =number +2;
+}
+// 1 to 20 evenNumber 
+for(var number=0 ; number<20; number=number+2 ){
+    console.log(number);
+}
+//   1 to 30 oddNumber
+for(var number =1; number<30; number=number + 2){
+   console.log(number)
+}
+
+// simples for-loop
+for(i=0;
+    i<25;
+    i++){
+        console.log("Display number of ")
+        console.log(i)
+    }
--- while-loop.js
+++ while-loop.js
@@ -5,21 +5,21 @@ while(RollCall<=21){
     RollCall++;
 }
 
-// Add Undivided  number 
+//1 to 100 All Undivided  number 
 var number =0;
 while(number < 100 && number/2){
     console.log(number)
     number ++;
 }
 
-// odd number
+// 1 to 100 all odd number
 var oddNumber = 1;
 while(oddNumber<100){
     console.log(oddNumber)
     oddNumber = oddNumber + 2;
 }
 
-// EvenNumber
+// 1 to 100 all EvenNumber
 var everNumber =0;
 while(everNumber<100){
     console.log(everNumber)
