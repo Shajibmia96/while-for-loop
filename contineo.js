e02ef58da75cb535c22c61437eb38869b48d8043
Shajib96 <shajibahamedornob@gmail.com>
2023-09-23 01:11:48 +0600

    Now know about continue

---
+++ contineo.js
@@ -0,0 +1,8 @@
+var numbers= [19,119,31,210,211,201,111,241,290,56,46,209]
+for(i=0; i<numbers.length; i++){
+    number=numbers[i]
+    if(number >200){
+        continue;
+    }
+    console.log(number)
+}
