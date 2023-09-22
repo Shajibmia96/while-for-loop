71e1353538e3ca39ee014e9eaacbc06be15452c9
Shajib96 <shajibahamedornob@gmail.com>
2023-09-22 11:46:04 +0600

    practies while and for loop

---
+++ while-loop.js
@@ -0,0 +1,27 @@
+var RollCall = 0;
+while(RollCall<=21){
+    console.log("Yes sir ")
+    console.log(RollCall)
+    RollCall++;
+}
+
+// Add Undivided  number 
+var number =0;
+while(number < 100 && number/2){
+    console.log(number)
+    number ++;
+}
+
+// odd number
+var oddNumber = 1;
+while(oddNumber<100){
+    console.log(oddNumber)
+    oddNumber = oddNumber + 2;
+}
+
+// EvenNumber
+var everNumber =0;
+while(everNumber<100){
+    console.log(everNumber)
+    everNumber = everNumber + 2;
+}
