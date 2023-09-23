d1e5fcf1cd25168b5898f8f9dd038f24784537fe
Shajib96 <shajibahamedornob@gmail.com>
2023-09-24 00:50:05 +0600

    practies some js problem

---
+++ practies-2.js
@@ -0,0 +1,61 @@
+// var num1='234';
+//  var num =parseInt(num1);
+
+//  var nums=parseFloat(num)
+//  console.log(num)
+// console.log(nums);
+
+// var num1 = '345'
+//  var num =parseInt('345');
+//  num1=parseInt('345')
+// console.log(num1)
+// display 39
+
+// for(i=0; i<=39; i++){
+//     console.log("AJke amar mon valo ney")
+// }
+    //  58 number to 98 number
+// var number=58;
+// while(number<=98){
+//     console.log(number);
+//     number++;
+// }
+
+//    Even number
+// for(number=412; number<=458; number+=2){
+//     console.log(number);
+// }
+
+// var learn = ['html', 'css', 'html5', 'css3', 'tailwind','boost']
+// console.log(learn);
+
+
+// var mobilePhones =['Samsung', 'iPhone','realmi', 'shawmi','blackvary'];
+// for(i=0; i<mobilePhones.length; i++){
+//    var mobilePhone =mobilePhones[i]
+//    console.log(mobilePhone);
+// }
+// for(i=30; i<=86; i++){
+//     console.log(i)
+//     if(i==50){
+//         break;
+//     }
+// }
+
+// var bookPrices = [104,564,626,202,199,231,442,100,100,50,75,95,57,865,85,]
+// for(i=0; i<bookPrices.length; i++){
+//     var bookPrice =bookPrices[i]
+//     if(bookPrice>200){
+//         continue;
+//     }
+//     console.log(bookPrice);
+// }
+
+
+var i = 0;
+
+for (i=0; i<5; i++){};
+
+console.log(i);
+
+  
